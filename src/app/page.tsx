c53eae1509e2b00cf9b0fb9d4aa4e6a7b16d4b7b
davidilerioluwa <davidilerioluwa1998@gmail.com>
2024-09-16 00:59:53 +0100

    improved padding for mobile at the top section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className=" bg-deepBlue w-screen h-full min-h-screen">
       {/* top section */}
-      <section className='w-full px-6 md:px-20 py-6'>
+      <section className='w-full px-6 md:px-20 pb-8 pt-2 md:py-6'>
         <div className='flex md:border border-grey500 rounded-3xl md:px-6 items-center justify-between'>
             <span className='p-2 text-xl md:text-3xl text-orange font-bold pr-4'>VUHOSI</span>
             <button className='bg-orange text-white text-sm font-bold rounded-3xl w-fit h-fit text-nowrap px-4 md:px-12 py-2'>Join Waitlist</button>
