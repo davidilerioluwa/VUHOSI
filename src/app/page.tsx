9319b2d54a3f61cec5cbf4eed60be5042bfdef28
David Godswill Ilerioluwa <76020967+davidilerioluwa@users.noreply.github.com>
2024-09-16 00:31:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
   return (
     <div className=" bg-deepBlue w-screen h-full min-h-screen">
       {/* top section */}
-      <div className='w-full px-6 md:px-20 py-6'>
+      <section className='w-full px-6 md:px-20 py-6'>
         <div className='flex md:border border-grey500 rounded-3xl md:px-6 items-center justify-between'>
             <span className='p-2 text-xl md:text-3xl text-orange font-bold pr-4'>VUHOSI</span>
             <button className='bg-orange text-white text-sm font-bold rounded-3xl w-fit h-fit text-nowrap px-4 md:px-12 py-2'>Join Waitlist</button>
         </div>
-      </div>
+      </section>
       
       {/* mid section */}
       <div className="flex flex-col w-full items-center justify-center md:p-4">
