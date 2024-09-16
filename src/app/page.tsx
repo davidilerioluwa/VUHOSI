a0418fb05339cc3fb9e4bf9188e8cb82fb701f04
davidilerioluwa <davidilerioluwa1998@gmail.com>
2024-09-16 13:15:47 +0100

    animated the gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import MidSection from "./MidSection";
+
+"use client"
+import { ReactTyped } from 'react-typed'
 
 export default function Home() {
   return (
@@ -12,7 +14,22 @@ export default function Home() {
       </section>
       
       {/* mid section */}
-    <MidSection/>
+      <section className="flex flex-col w-full items-center justify-center md:p-4">
+        <img src="logo.png" className="w-16 mb-2"/>
+        <p className="gradient-animation slideRightIn text-3xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-orange to-lightOrange">AI POWERED INSIGHTS</p>
+        <p  className="slideLeftIn text-3xl md:text-6xl font-bold text-white text-center" >For Startups And Investors</p>
+        <p className="text-grey300 text-center px-4 md:px-40 lg:px-60 xl:px-80 my-3 text-sm">Optimise your startup with critical intelligence to help identify risks and opportunities, guiding you to develop market-fit products, build strong teams, and scale your startup confidently</p>
+        <div className="w-full flex flex-col items-center justify-center">
+          {/* searchbox */}
+          <div className="w-80 md:w-96 md:border border-grey500 flex flex-col gap-4 md:gap-2 md:flex-row p-1 rounded-3xl items-center justify-center">
+              <input type="text" placeholder="Enter Your Email Address" className="text-sm text-grey500 border border-grey500 md:border-none p-4 md:p-2 bg-deepBlue outline-none w-full rounded-3xl"/>
+              <button className="text-nowrap bg-orange text-white w-fit px-6 py-3 rounded-3xl text-sm">Join Waitlist</button>
+          </div>
+          <label className="text-grey400 text-sm w-full text-center hidden md:block">
+              Join our waitlist and get early access when we launch
+          </label>
+        </div>
+      </section>
 
       {/* lower section */}
       <section className="px-6 md:px-20 lg:px-40 xl:px-60 pt-6 md:pt-2  pb-4">
